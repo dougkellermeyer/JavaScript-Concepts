aab01146415ba5b9590a6cc268436ac8adcb46bc
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-25 16:25:14 -0400

    covered bind

---
+++ callApplyBind/apply.js
@@ -0,0 +1,18 @@
+//apply is very similar to call() except that takes an array of args rather than explicitly stated like in call()
+
+//format is function.apply(thisArg, [args])
+
+function applyMe(...args){
+    console.log(...args) //
+    console.log(this.a) // 4
+  }
+  
+  var obj = {
+  a: 4
+  }
+
+  array = [1,2,3,4]
+  
+  applyMe.apply(obj,array)
+
+  
---
+++ callApplyBind/bind.js
@@ -0,0 +1,21 @@
+//the bind method is a little bit different than call and apply
+
+//bind does NOT execute right away, it creates a new function that is hard bound to the object we assign as `this`
+
+//format is function.bind(thisArg)
+
+var obj = {
+    greeting: "hi",
+    speak: function(){
+        console.log(this.greeting)
+    },        
+}
+
+obj.speak() // => "hi"
+
+var talkFunction = obj.speak
+talkFunction(); // => undefined - we've lost our `this` binding
+
+talkFunction.bind(obj); //undefined
+var boundTalkFunction = talkFunction.bind(obj);
+boundTalkFunction() // => "hi"
