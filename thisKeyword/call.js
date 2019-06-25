cfc543c0dd55eee187b6e0618d07b9bef13c848b
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-25 13:53:51 -0400

    moved somethings around

--- thisKeyword/call.js
+++ thisKeyword/call.js
@@ -20,21 +20,4 @@ var someoneElse = {
 identify.call(me) // => "Doug"
 identify.call(someoneElse) // => "John"
 
-//-------another call() example--------
-
-function callMe(a,b,c){
-  console.log(a,b,c)
-  console.log(this.a) // 4
-}
-
-var obj = {
-a: 4
-}
-
-callMe.call(obj,1,2,3)
-
-
-
-
-// callMe.call(obj,1,2,3) // we are binding obj's `this` to callMe()
 
