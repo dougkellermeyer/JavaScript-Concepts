6c427d85ad49175a15b97aa955d54fa81c3ce1f3
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-25 11:07:52 -0400

    done with call

---
+++ callApplyBind.js
@@ -0,0 +1,21 @@
+//call, apply, and bind are very useful functions for being able to define `this` and passing args when invoking functions
+
+//let's take a look at call first. It's format is function.call(thisArg, arg1, arg2, ...)
+
+function identify(){
+  var response = [this.name, this.age];
+  console.log(response);
+}
+
+var me = {
+    name: "Doug",
+    age: 26
+}
+
+var someoneElse = {
+    name: "John",
+    age: 30
+}
+
+identify.call(me) // => "Doug"
+identify.call(someoneElse) // => "John"
