8f48c96d593bc6951aa94b20603e70c71422902e
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-07-10 16:36:02 -0400

    built in Constructors

--- builtInConstructors.js
+++ builtInConstructors.js
@@ -6,7 +6,9 @@ const name2 = new String('Doug')
 // console.log(name1) => 'Doug'
 // console.log(name2) => [String: 'Doug']
 
-//it's important to note that although name2'value is still 'Doug', it is technically an object, NOT a string like name1 
+//it's important to note that although name2'value is still 'Doug', 
+//it is technically an object, NOT a string like name1 
+
 console.log(typeof name1) // 'string'
 console.log(typeof name2) // 'object'
 
@@ -26,4 +28,45 @@ if(name2 === 'Doug'){
     console.log('nope')
 }
 
-//this prints a 'nope', because despite the value of name2 being equal to 'Doug', it is NOT a string but rather an object
+//this prints a 'nope', because despite the value of name2 being equal to 'Doug', 
+//it is NOT a string but rather an object
+
+//Number 
+
+const num1 = 5;
+const num2 = new Number(5);
+
+console.log(typeof num2); // => object
+
+//Boolean
+
+const boo1 = true;
+const boo2 = new Boolean(true); // => object
+
+console.log(typeof boo2);
+
+//Function
+
+const getSum1 = function(x,y){
+    return x + y
+};
+
+const getSum2 = new Function('x','y', 'return 1 + 1')
+console.log(getSum2(1,1))
+
+//Object
+
+const john = {name: 'John'};
+const john1 = new Object({name:'John'});
+console.log(john1)
+
+//Array 
+const arr1 = [1,2,3];
+const arr2 = new Array(1,2,3)
+console.log(arr1,arr2)
+
+//Regex
+const reg1 = /\w+/
+const reg2 = new RegExp('\\w+')
+console.log(reg1)
+console.log(reg2)
