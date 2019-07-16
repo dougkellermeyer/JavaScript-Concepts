1c91b8b43331d53da9fcd5820ec2c476310a5f3e
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-07-16 16:23:45 -0400

    more good stuff

--- prototypicalInheritance.js
+++ prototypicalInheritance.js
@@ -11,10 +11,13 @@ Person.prototype.greeting = function(){
 
 const person1 = new Person('Doug','Kellermeyer');
 
-// console.log(person1.greeting());
+console.log(person1.greeting());
 
 //Inherit the Person prototype methods
 Customer.prototype = Object.create(Person.prototype);
+
+//Make customer.prototype return Customer
+Customer.prototype.constructor = Customer;
 
 //Create a customer
 
@@ -27,4 +30,10 @@ function Customer(firstName, lastName, phone, membership){
 const customer1 = new Customer('Bill', 'Smith', '555-555-5555', 'Standard')
 console.log(customer1)
 
-console.log(custom1.greeting())
+//Customer greeting
+Customer.prototype.greeting = function(){
+    return `hi there ${this.firstName} ${this.lastName} welcome to our company`
+}
+
+//can overwrite the greeting
+console.log(customer1.greeting())
