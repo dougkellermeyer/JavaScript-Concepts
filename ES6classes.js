2ff31527ecad2577cb8884739379efcff489e1a7
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-07-23 16:07:13 -0400

    classes are sure a thing

--- ES6classes.js
+++ ES6classes.js
@@ -1,2 +1,31 @@
 //using ES6 to build objects and classes
 
+class Person {
+    constructor(firstName, lastName, dob){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dob)
+    }
+
+    greeting(){
+        return `Hi there ${this.firstName} ${this.lastName}`
+    }
+
+    calculateAge(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970)
+    }
+
+    //static method
+    static addNumbers(x,y){
+        return x + y;
+    }
+}
+
+const doug = new Person('Doug','Kellermeyer', '12-23-1992');
+
+console.log(doug.greeting());
+console.log(doug.calculateAge());
+
+console.log(Person.addNumbers(1,2))
