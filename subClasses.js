022ad99dc7185a3594e2cc60aad8387686be429c
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-07-23 16:37:05 -0400

    subclasses

---
+++ subClasses.js
@@ -0,0 +1,24 @@
+class Person {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting(){
+        return `Yo ${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        //super call the parents class construtor
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.membership = membership;
+    }
+}
+
+const john = new Customer('John', 'Smith', '555-555-5555', 'Standard');
+
+console.log(john)
