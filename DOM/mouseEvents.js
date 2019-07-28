a79f813d337276f8cc9056c7b60d2e42eed2a374
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-07-28 09:49:31 -0400

    added another button for funzies

--- DOM/mouseEvents.js
+++ DOM/mouseEvents.js
@@ -4,8 +4,14 @@ var clearButton = document.querySelector('.clear-tasks');
 var card = document.querySelector('.card');
 var heading = document.querySelector('h5');
 
+// var searchButton = document.querySelector('.search');
+
 //Click
 // clearButton.addEventListener('click', runEvent);
+
+// searchButton.addEventListener('click', function(){
+//     console.log('well this is kinda neat, its a UI working with js')
+// });
 
 //DoubleCLick 
 // clearButton.addEventListener('dblclick', runEvent)
