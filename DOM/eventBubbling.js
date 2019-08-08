75d02fb157e7764bd2acca32c2aa073172f27ed1
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-08-08 15:02:06 -0400

    event delegation

--- DOM/eventBubbling.js
+++ DOM/eventBubbling.js
@@ -1,21 +1,44 @@
-//EVENT BUBBLING
+//EVENT BUBBLING ((putting the event listening on a child element and going or "bubbling" up the DOM))
 
-document.querySelector('.card-title').addEventListener('click', 
-function(){
-    console.log('card title')
-})
+// document.querySelector('.card-title').addEventListener('click', 
+// function(){
+//     console.log('card title')
+// })
 
-document.querySelector('.card-content').addEventListener('click', 
-function(){
-    console.log('card content')
-})
+// document.querySelector('.card-content').addEventListener('click', 
+// function(){
+//     console.log('card content')
+// })
 
-document.querySelector('.card').addEventListener('click', 
-function(){
-    console.log('card')
-})
+// document.querySelector('.card').addEventListener('click', 
+// function(){
+//     console.log('card')
+// })
 
-document.querySelector('.col').addEventListener('click', 
-function(){
-    console.log('col')
-})
+// document.querySelector('.col').addEventListener('click', 
+// function(){
+//     console.log('col')
+// })
+
+//EVENT DELEGATION (putting the event listening on a parent element and going down the DOM)
+//find the element you want to find using e.target to see what you're clicking on
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem)
+
+function deleteItem(e){
+    // console.log(e.target)
+    // if(e.target.parentElement.className === 'delete-item secondary content'){
+    //     console.log('this aint it chief')
+    // }
+
+    //we use contains here instead of className, as className could change and it would no
+    //target the right element
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log('yeet');
+        e.target.parentElement.parentElement.remove();
+    }
+}
