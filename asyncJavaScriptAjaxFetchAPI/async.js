26635900364a5ebe8c769c86647effba315048f7
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-08-09 11:21:26 -0400

    ajax refresh

---
+++ asyncJavaScriptAjaxFetchAPI/async.js
@@ -0,0 +1,7 @@
+//lets go over what asynchronous code is and how it differs from synchronous code
+
+//asynchronous or async code doesn't have to wait for a request or function to finish to run the next function or section of code
+
+//AJAX (Asynchronous Javascript And Xml)- set of web tech that sends and recieves data asynchronously. Doesn't interfere with the current page. 
+//-> ironically enough, although XML is part of the AJAX acronym, XML is rarely used today - it's nearly always JSON
+
