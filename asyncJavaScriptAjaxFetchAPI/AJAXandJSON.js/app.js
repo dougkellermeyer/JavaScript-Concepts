2ad60a5fde7291ba342ecc8ce7bae14a8ff4b23e
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-08-14 16:56:54 -0400

    JSON is pretty handy:

---
+++ asyncJavaScriptAjaxFetchAPI/AJAXandJSON.js/app.js
@@ -0,0 +1,29 @@
+document.getElementById('button1').addEventListener('click',loadCustomer);
+
+function loadCustomer(e){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.onload = function(){
+    if(this.status === 200){
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+      //now lets take the JSON and slap it on the page
+      const output = 
+      `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `
+
+      document.getElementById('customer').innerHTML = output;
+    };
+  }
+
+  xhr.send();
+};
