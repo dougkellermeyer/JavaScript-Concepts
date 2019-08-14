1f5223e691b651dff305950fb52456f8cffcc3a6
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-08-14 16:24:55 -0400

    more ajax

--- asyncJavaScriptAjaxFetchAPI/async.js
+++ asyncJavaScriptAjaxFetchAPI/async.js
@@ -5,3 +5,6 @@
 //AJAX (Asynchronous Javascript And Xml)- set of web tech that sends and recieves data asynchronously. Doesn't interfere with the current page. 
 //-> ironically enough, although XML is part of the AJAX acronym, XML is rarely used today - it's nearly always JSON
 
+//There are other ways to make HTTP requests
+//-Fetch API (comes for free in vanilla JS), Axios, Superagent, jQuery, Node HTTP
+
