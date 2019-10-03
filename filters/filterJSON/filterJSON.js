7b4c80ca6412f5ce202ca5e387089e93f26c93c7
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-10-03 10:44:07 -0400

    data hooked up:

--- filters/filterJSON/filterJSON.js
+++ filters/filterJSON/filterJSON.js
@@ -1,1 +1,22 @@
-console.log("filterJSON is hooked up");
+document.getElementById('getDataButton').addEventListener('click', loadData);
+
+function loadData(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'gameData.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            document.getElementById('outputData').innerHTML = 
+            `<p>${this.responseText}</p>`;
+        }
+    };
+
+    xhr.onerror = function(){
+        console.log('An error occurred on the request')
+    };
+
+    xhr.send();
+
+};
+
