500a9b7a2f3e407da9bd448003684ccb6b99fc2c
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-10-03 11:30:34 -0400

    adding filter logic

--- filters/filterJSON/filterJSON.js
+++ filters/filterJSON/filterJSON.js
@@ -6,9 +6,25 @@ function loadData(){
     xhr.open('GET', 'gameData.json', true);
 
     xhr.onload = function(){
+      
         if(this.status === 200){
+            let res = JSON.parse(this.responseText);
+
+            //filterBy function would go here
+            function filterBy(response){
+                this.response = response;
+                console.log(response)
+            }
+
+            filterBy(res);
+            
+            let filteredRes = res.filter(function(game){
+                return game.homeTeam;
+            });
+            console.log(filteredRes)
+            //put data on the page
             document.getElementById('outputData').innerHTML = 
-            `<p>${this.responseText}</p>`;
+            `<p>${filteredRes[0].homeTeam}</p>`;
         }
     };
 
