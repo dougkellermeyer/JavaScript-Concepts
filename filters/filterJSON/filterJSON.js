57c0cd921f2f31a87a399a8d2a582c0efcc42f34
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-10-03 14:05:53 -0400

    updated dropdown with divisions

--- filters/filterJSON/filterJSON.js
+++ filters/filterJSON/filterJSON.js
@@ -42,8 +42,25 @@ function loadData(){
     // return res
 };
 
+
+
 //invoke our function so the data is available 
 // loadData()
 
+//create an IIFE to grab the divison select options
+(function(){
+    var divisionsArr = ["AFC East", "AFC West", "AFC North", "AFC South", "NFC East", "NFC West", "NFC North", "NFC South"];
+    var division = document.getElementById("division"),
+    df = document.createDocumentFragment();
 
+    //loop through the divisions
+    //TODO replace the hardcoded 7 with lenght of the division array
+    for (let i = 0; i <= divisionsArr.length; i++){
+        var divisionOption = document.createElement('option');
+        divisionOption.value = i;
+        divisionOption.appendChild(document.createTextNode(divisionsArr[i]));
+        df.appendChild(divisionOption);
+    }
+    division.appendChild(df);
+}())
 
